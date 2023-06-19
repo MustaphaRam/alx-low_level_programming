39bc142c200ff617b0f5a2af9ff9ea5bceaf0da8
MustaphaRam <mustapha.mad.75@gmail.com>
2023-06-19 23:29:33 +0100

    tasks 0x18 C Dynamic libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/win.so https://raw.github.com/MustaphaRam/alx-low_level_programming/master/master/0x18-dynamic_libraries/win.so
+wget -P /tmp https://github.com/MustaphaRam/alx-low_level_programming/blob/main/0x18-dynamic_libraries/101-make_me_win.sh
 export LD_PRELOAD=/tmp/win.so
